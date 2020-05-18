528dfcb5587bbaecd923c8a3f9cf337deefea92c
Esa <diapaasi@gmail.com>
2020-05-18 14:26:09 +0300

    WIFI and OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,62 @@
 #include <Arduino.h>
+#include "WiFi.h"
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #include <config.h>
 
 void setup()
 {
   Serial.begin(9600);
+
+  WiFi.begin(SSID, PWD);
+  Serial.println("Wifi connecting");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(5000);
+  }
+
+  Serial.println("Wifi connected");
+  Serial.println(WiFi.localIP());
+
+  // https://docs.platformio.org/en/latest/platforms/espressif32.html#using-built-in-local-solution
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() {
+        Serial.println("\nEnd");
+      })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          Serial.println("End Failed");
+      });
+
+  ArduinoOTA.begin();
 }
 
 void loop()
 {
-  Serial.println("Hello world!");
-  delay(1000);
+  ArduinoOTA.handle();
 }
