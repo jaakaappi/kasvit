29e661ecc988fbcf405ab3b2404b1e7f5251ccee
Esa <diapaasi@gmail.com>
2020-05-30 11:04:59 +0300

    Image capture draft

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,36 @@
 #include <time.h>
 #include <HTTPClient.h>
 #include "Arduino_JSON.h"
+#include "esp_camera.h"
+#include "soc/soc.h"          // Disable brownout problems
+#include "soc/rtc_cntl_reg.h" // Disable brownout problems
+#include <SPIFFS.h>
+#include <FS.h>
+#include "img_converters.h"
+#include "driver/rtc_io.h"
 
 #include <config.h>
+
+// Photo File Name to save in SPIFFS
+#define FILE_PHOTO "/photo.jpg"
+
+// OV2640 camera module pins (CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 0;
@@ -25,6 +53,9 @@ void setup()
 
   Serial.begin(9600);
   Serial.println(esp_sleep_get_wakeup_cause());
+
+  // Turn-off the 'brownout detector'
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 
   // === CONNECT WIFI ===
 
@@ -104,7 +135,110 @@ void setup()
 
   if (ota_disabled)
   {
-    // === TAKE PICTURE ===
+    // === MOUNT FILESYSTEM, TAKE PICTURE ===
+
+    // https://randomnerdtutorials.com/esp32-cam-take-photo-display-web-server/
+
+    if (!SPIFFS.begin(true))
+    {
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      ESP.restart();
+    }
+    else
+    {
+      delay(500);
+      Serial.println("SPIFFS mounted successfully");
+    }
+
+    // Turn-off the 'brownout detector'
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+    // OV2640 camera module
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+
+    if (psramFound())
+    {
+      config.frame_size = FRAMESIZE_UXGA;
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+    }
+    else
+    {
+      config.frame_size = FRAMESIZE_SVGA;
+      config.jpeg_quality = 12;
+      config.fb_count = 1;
+    }
+
+    // Camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK)
+    {
+      Serial.printf("Camera init failed with error 0x%x", err);
+      ESP.restart();
+    }
+
+    camera_fb_t *fb = NULL; // pointer
+    bool ok = 0;            // Boolean indicating if the picture has been taken correctly
+
+    do
+    {
+      // Take a photo with the camera
+      Serial.println("Taking a photo...");
+
+      fb = esp_camera_fb_get();
+      if (!fb)
+      {
+        Serial.println("Camera capture failed");
+        return;
+      }
+
+      // Photo file name
+      Serial.printf("Picture file name: %s\n", FILE_PHOTO);
+      File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
+
+      // Insert the data in the photo file
+      if (!file)
+      {
+        Serial.println("Failed to open file in writing mode");
+      }
+      else
+      {
+        file.write(fb->buf, fb->len); // payload (image), payload length
+        Serial.print("The picture has been saved in ");
+        Serial.print(FILE_PHOTO);
+        Serial.print(" - Size: ");
+        Serial.print(file.size());
+        Serial.println(" bytes");
+      }
+      // Close the file
+      file.close();
+      esp_camera_fb_return(fb);
+
+      // check if file has been correctly saved in SPIFFS
+      File f_pic = SPIFFS.open(FILE_PHOTO);
+      unsigned int pic_sz = f_pic.size();
+      ok = pic_sz > 100;
+    } while (!ok);
 
     // === SEND PICTURE OVER HTTP ===
 
@@ -114,6 +248,7 @@ void setup()
 
     http.begin(serverPath.c_str());
     http.addHeader("Content-Type", "application/json");
+    // TODO make this monstrosity prettier
     JSONVar requestObject;
     char isoTimeString[200];
     strftime(isoTimeString, sizeof(isoTimeString), "%FT%T+03:00", &timeinfo);
@@ -134,7 +269,7 @@ void setup()
     }
     http.end();
 
-    // SET SLEEP
+    // === GO SLEEP ===
 
     struct tm next_time = timeinfo;
     // next_time.tm_mday = timeinfo.tm_mday == 31 ? 1 : next_time.tm_mday + 1;
