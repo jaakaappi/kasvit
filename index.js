5cd09e575bdecbb3c5d8768f529719058f2319f5
Esa <diapaasi@gmail.com>
2020-06-04 22:14:13 +0300

    Image listing

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ app.use(bodyParser.raw({ limit: "500kb" }));
 
 app.post("/images", (req, res) => {
   const timestamp = req.get("Picture-FileName");
-  if (!fs.exists("/images")) fs.mkdir("/images");
-  fs.writeFile(`./images/${timestamp}.jpg`, req.body, function (err) {
+  if (!fs.exists("/images", () => {})) fs.mkdir("./images", () => {});
+  fs.writeFile(`./images/${timestamp}.jpg`, req.body, (err) => {
     if (err) return console.log(err);
   });
   res.sendStatus(200);
+});
+
+app.get("/images", (req, res) => {
+  if (!fs.exists("/images", () => {})) fs.mkdir("./images", () => {});
+  const files = fs.readdirSync("./images");
+  console.log(files);
+  res.send({ ids: files });
 });
 
 app.listen(port, () => {});
