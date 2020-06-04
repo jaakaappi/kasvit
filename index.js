77f49493835612f99da0f7d69b29d18590683ce6
Esa <diapaasi@gmail.com>
2020-06-04 21:57:20 +0300

    Picture saving, probably not safe

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.raw({ limit: "500kb" }));
+
 app.post("/images", (req, res) => {
+  const timestamp = req.get("Picture-FileName");
+  if (!fs.exists("/images")) fs.mkdir("/images");
+  fs.writeFile(`./images/${timestamp}.jpg`, req.body, function (err) {
+    if (err) return console.log(err);
+  });
   res.sendStatus(200);
 });
 
