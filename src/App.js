838813040d9d2013843c16a494ee60654c3730d2
Esa <diapaasi@gmail.com>
2020-06-07 00:42:09 +0300

    Image page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">Image list goes here</div>;
+  const [imagesNames, setImageNames] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/images")
+      .then((response) => {
+        setImageNames(response.data.ids);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  return (
+    <div>
+      {imagesNames.map((image) => {
+        return (
+          <div key={image}>
+            <p>{image}</p>
+            <img src={`http://localhost:3001/images/${image}`} alt={image} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
