d40f91bc897a22307297a98b525359374731e6d5
Esa <diapaasi@gmail.com>
2020-06-07 00:41:36 +0300

    Image provider API

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const express = require("express");
 const fs = require("fs");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
+app.use(cors());
 app.use(bodyParser.raw({ limit: "500kb" }));
+app.use(express.static("public"));
 
-app.post("/images", (req, res) => {
+app.post("/api/images", (req, res) => {
   const timestamp = req.get("Picture-FileName");
   if (!fs.exists("/images", () => {})) fs.mkdir("./images", () => {});
   fs.writeFile(`./images/${timestamp}.jpg`, req.body, (err) => {
@@ -16,10 +19,10 @@ app.post("/images", (req, res) => {
   res.sendStatus(200);
 });
 
-app.get("/images", (req, res) => {
-  if (!fs.exists("/images", () => {})) fs.mkdir("./images", () => {});
-  const files = fs.readdirSync("./images");
-  console.log(files);
+app.get("/api/images", (req, res) => {
+  if (!fs.exists("./public/images", () => {}))
+    fs.mkdir("./public/images", () => {});
+  const files = fs.readdirSync("./public/images");
   res.send({ ids: files });
 });
 
