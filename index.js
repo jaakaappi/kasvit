17bcc5ba12d6c6cd4d346c97dcad854f009a730e
Esa <diapaasi@gmail.com>
2020-06-24 19:13:42 +0300

    Filenames to use epoch timestamp value

--- index.js
+++ index.js
@@ -19,14 +19,19 @@ const pwd = process.env.API_USER_PWD;
 console.log(user, pwd);
 
 app.post("/api/images", basicAuth({ users: { [user]: pwd } }), (req, res) => {
-  const timestamp = req.get("Picture-FileName");
+  const sender = req.get("Picture-FileName");
   if (!fs.existsSync("./public/images"))
     fs.mkdir("./public/images", (err) => {
       console.error(err);
     });
-  fs.writeFile(`./public/images/${timestamp}.jpg`, req.body, (err) => {
-    if (err) return console.log(err);
-  });
+  //console.log(req);
+  fs.writeFile(
+    `./public/images/${sender} ${Date.now()}.jpg`,
+    req.body,
+    (err) => {
+      if (err) return console.log(err);
+    }
+  );
   res.sendStatus(200);
 });
 
@@ -34,7 +39,27 @@ app.get("/api/images", (req, res) => {
   if (!fs.existsSync("./public/images")) res.send({ ids: [] });
   else {
     const files = fs.readdirSync("./public/images");
-    res.send({ ids: files });
+    res.send({
+      ids: files
+        .sort((first, second) => {
+          first = first
+            .split(" ")
+            .slice(1)
+            .join(" ")
+            .split(".")
+            .slice(0, -1)
+            .join(" ");
+          second = second
+            .split(" ")
+            .slice(1)
+            .join(" ")
+            .split(".")
+            .slice(0, -1)
+            .join(" ");
+          return first - second;
+        })
+        .reverse(),
+    });
   }
 });
 
